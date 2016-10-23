32b3e078f2657d364e14cf4b76e03273c7c8e73a
Jay MOULIN <jaymoulin@gmail.com>
2016-10-23 13:05:16 +0200

    fix PHP container port exposure

--- docker.sh
+++ docker.sh
@@ -14,18 +14,19 @@ docker build -t querymail-img-php $parent_path/docker
 title "run docker php container"
 docker rm -f querymail-php 2> /dev/null
 docker run --name querymail-php \
-		-v $parent_path:/srv/http \
-		-v $parent_path/docker/ssmtp.conf:/etc/ssmtp/ssmtp.conf \
-		--restart=always \
-		-d querymail-img-php
+        -v $parent_path:/srv/http \
+        -v $parent_path/docker/ssmtp.conf:/etc/ssmtp/ssmtp.conf \
+        -p 9000:9000 \
+        --restart=always \
+        -d querymail-img-php
 
 title "nginx container"
 docker rm -f querymail-nginx 2> /dev/null
 docker run --name querymail-nginx \
-		-v $parent_path:/usr/share/nginx:ro \
-		-v $parent_path/docker/nginx.conf:/nginx.conf \
-		--restart=always \
-		-p 80:80 \
-		--link querymail-php \
-		-d nginx \
-		nginx -c /nginx.conf
+        -v $parent_path:/usr/share/nginx:ro \
+        -v $parent_path/docker/nginx.conf:/nginx.conf \
+        --restart=always \
+        -p 80:80 \
+        --link querymail-php \
+        -d nginx \
+        nginx -c /nginx.conf
